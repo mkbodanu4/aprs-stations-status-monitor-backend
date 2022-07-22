214ba95603f0ce01aadb4e1b492043c8b3720c63
mkbodanu4 <mailmanbo@gmail.com>
2022-07-22 15:07:51 +0300

    Added logging and updated database structure

--- igate_status_monitor.py
+++ igate_status_monitor.py
@@ -1,9 +1,13 @@
 import mysql.connector
 import aprslib
 import yaml
+import logging
+import time
 
 with open("configuration.yaml", 'r') as stream:
     configuration = yaml.safe_load(stream)
+
+logging.basicConfig(level=configuration['logging']['level'])
 
 if configuration['mysql']['unix_socket']:
     db = mysql.connector.connect(
@@ -21,15 +25,30 @@ else:
     )
 
 target_call_signs = []
-crs = db.cursor()
-crs.execute("SELECT * FROM `call_signs`;")
-result = crs.fetchall()
-for call_sign in result:
-    target_call_signs.append(call_sign[1])
-crs.close()
+last_targets_update = 0
+
+
+def update_targets():
+    global target_call_signs, last_targets_update
+
+    target_call_signs = []
+    crs = db.cursor()
+    crs.execute("SELECT `value` FROM `call_signs`;")
+    result = crs.fetchall()
+    for call_sign in result:
+        target_call_signs.append(call_sign[0])
+    crs.close()
+
+    logging.info("Target call signs list updated")
+    last_targets_update = time.time()
 
 
 def callback(packet):
+    global last_targets_update
+
+    if (time.time() - last_targets_update) > 60:
+        update_targets()
+
     try:
         parsed = aprslib.parse(packet)
     except (aprslib.ParseError, aprslib.UnknownFormat) as exp:
@@ -47,18 +66,70 @@ def callback(packet):
             crs.close()
 
             if call_sign_db:
+                query = """
+                    INSERT INTO
+                        `status` (
+                            `call_sign_id`,
+                            `date`,
+                            `from`,
+                            `via`,
+                            `path`,
+                            `symbol`,
+                            `symbol_table`,
+                            `latitude`,
+                            `longitude`
+                        )
+                    VALUES
+                        (
+                            %s,
+                            UTC_TIMESTAMP(),
+                            %s,
+                            %s,
+                            %s,
+                            %s,
+                            %s,
+                            %s,
+                            %s
+                        )
+                    ON DUPLICATE KEY
+                        UPDATE
+                            `date`=UTC_TIMESTAMP(),
+                            `from` = %s,
+                            `via` = %s,
+                            `path` = %s,
+                            `symbol` = %s,
+                            `symbol_table` = %s,
+                            `latitude` = %s,
+                            `longitude` = %s
+                ;"""
+                params = (
+                    call_sign_db[0],
+                    parsed.get('from'),
+                    parsed.get('via'),
+                    ','.join(parsed.get('path')),
+                    parsed.get('symbol'),
+                    parsed.get('symbol_table'),
+                    parsed.get('latitude'),
+                    parsed.get('longitude'),
+                    parsed.get('from'),
+                    parsed.get('via'),
+                    ','.join(parsed.get('path')),
+                    parsed.get('symbol'),
+                    parsed.get('symbol_table'),
+                    parsed.get('latitude'),
+                    parsed.get('longitude')
+                )
+
                 crs = db.cursor()
-                crs.execute(
-                    "INSERT INTO `status`(`call_sign_id`, `date_last_heard`, `path`, `date_refreshed`) VALUES(%s, UTC_TIMESTAMP(), %s, UTC_TIMESTAMP()) ON DUPLICATE KEY UPDATE `date_last_heard`=UTC_TIMESTAMP(), `path` = %s, `date_refreshed` = UTC_TIMESTAMP();",
-                    (call_sign_db[0], ','.join(parsed.get('path')), ','.join(parsed.get('path'))))
+                crs.execute(query, params)
                 db.commit()
                 crs.close()
 
-                # print(call_sign + " saved")
-            # else:
-                # print(call_sign + " ignored")
-    # else:
-        # print(parsed.get('from') + " ignored")
+                logging.info("Call sign " + call_sign + " data updated")
+            else:
+                logging.info("Call sign " + call_sign + " data ignored")
+    else:
+        logging.info("Call signs " + ', '.join(test_call_signs) + " ignored")
 
 
 AIS = aprslib.IS(configuration['aprs']['callsign'], passwd="-1", host=configuration['aprs']['host'], port=14580)
